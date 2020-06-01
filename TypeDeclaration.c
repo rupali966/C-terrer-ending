a38c54dc5ca22daa30eb436d80881f30ffab579d
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-01 18:34:32 +0530

    that was a example of the c type Declaration
    how to declare the variable you can learn from that

---
+++ TypeDeclaration.c
@@ -0,0 +1,12 @@
+#include<stdio.h> // it is also requered 
+void main() // the main function the system start from compiling for that function  that function
+{
+	float x , y ;
+  // hare declare the variable
+	int code;
+	short int count;
+	double deveation;
+	char c;
+  // that program have no out put becuse ve just declare variable 
+  // but dosent do anything means Operations
+}
