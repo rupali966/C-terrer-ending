c702c58ae81da837b25a760263193241654db029
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-07 12:19:56 +0530

    Create StorageDeclaration.c

---
+++ StorageDeclaration.c
@@ -0,0 +1,13 @@
+#include<stdio.h> // it is also requered becuse we called all function for this library
+void main() // the main function the system start from compiling for that function  that function
+{
+	auto float x , y ;
+	// hare declare the first storage-class then data-type  and than variable
+	static  int code;
+	// starageclass datatype variable;
+	register short int count;
+	extern double deveation;
+	auto char c;
+}
+  // that program have no out put becuse ve just declare variable 
+  // but dosent do anything means Operations
