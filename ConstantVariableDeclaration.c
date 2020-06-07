784f12fc403121e6d3a339af30429f9f200c4001
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-07 12:12:59 +0530

    Create ConstantVariableDeclaration.c

---
+++ ConstantVariableDeclaration.c
@@ -0,0 +1,14 @@
+#include<stdio.h> // it is also requered becuse we called all function for this library
+void main() // the main function the system start from compiling for that function  that function
+{
+	 //  const variables is declare 
+	 // const datatype variable = Constantvalue
+	 const double y =10.0;
+	 const double q = 12.0;
+	 const float x = 0.22;
+	 // const + datatype + variable = asignValue;
+	 
+// that program have no out put becuse ve just declare variable 
+// but dosent do anything means Operations
+
+}
