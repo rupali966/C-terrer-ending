ab684c6dc059b5ff963b63b2b861dd33c98a9ec5
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-07 12:21:32 +0530

    Create TypeDefDeclaration.c

---
+++ TypeDefDeclaration.c
@@ -0,0 +1,14 @@
+#include<stdio.h> // it is also requered becuse we called all function for this library
+void main() // the main function the system start from compiling for that function  that function
+{
+	typedef float realnum , imgnum;
+	// typedef + datatype + identifier 
+	typedef int code, prgrams;
+	realnum num1, num2;
+	// identifier + variable
+	imgnum inum1, inum2;
+	code programming;
+	prgrams nocoding;
+}
+  // that program have no out put becuse ve just declare variable 
+  // but dosent do anything means Operations
