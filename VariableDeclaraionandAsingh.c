468b404b378c1bac98f19e01796c490031eb21ad
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-07 12:18:57 +0530

    Create VariableDeclaraionandAsingh.c

---
+++ VariableDeclaraionandAsingh.c
@@ -0,0 +1,18 @@
+#include<stdio.h> // it is also requered becuse we called all function for this library
+void main() // the main function the system start from compiling for that function  that function
+{
+	 // hare Only declared the variables
+	 // datatype variable, variable2 ;
+	 double y, q;
+	 float x , p;
+	 int k;
+	 // declaration And Assigment
+	 int m = 54321;
+	 long int n = 1234567890;
+	 // only assigment 
+	 x = 1.23;
+	 y = 9.8;
+	 k = 54321;
+	 p = q = 1.0; // hare is trick part but simple p and q is 1.0 simple
+
+}
