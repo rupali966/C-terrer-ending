88e8362e600ebbf8098bf769cd8a6bf53aacd3a5
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-07 12:17:55 +0530

    Create EnumVariableDeclaration.c

---
+++ EnumVariableDeclaration.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // it is also requered becuse we called all function for this library
+void main() // the main function the system start from compiling for that function  that function
+{
+	enum day { sunday, monday, tuesday, wendesday, tharsday, friday , satarday };
+	// hare declare enum identifier name
+	// enum identifier {Values};
+	//enum identifier variable;
+	// variable = values in barace
+	enum day week_start, week_end;
+	// hare we declare the enum variable
+	week_start = sunday;     // hare assignment of enums identifier values
+	week_end = satarday;  // hare assignment of enums identifier values
+
+
+  // that program have no out put becuse ve just declare variable 
+  // but dosent do anything means Operations
+}
