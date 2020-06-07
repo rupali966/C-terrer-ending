3403e3e4c85cf48d2202d622cb4bb69d86201c5b
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-07 12:22:14 +0530

    Create VolatileVariableDeclaration.c

---
+++ VolatileVariableDeclaration.c
@@ -0,0 +1,12 @@
+#include<stdio.h> // it is also requered becuse we called all function for this library
+void main() // the main function the system start from compiling for that function  that function
+{
+	 //  volitile(changeble) variables is declare 
+	 volatile double y =10.0;
+	 volatile double q = 12.0;
+	 volatile float x = 0.22;
+	 // volatile + datatype + variable;
+
+}
+  // that program have no out put becuse ve just declare variable 
+  // but dosent do anything means Operations
