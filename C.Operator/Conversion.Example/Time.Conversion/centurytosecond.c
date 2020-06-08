6e5b42f2a9f95b12ddcafb9ada1e5af13ef05378
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-08 18:04:28 +0530

    Create centurytosecond.c

---
+++ C.Operator/Conversion.Example/Time.Conversion/centurytosecond.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // hare call the standerd input output library for input and output
+int main() // hare is the main funnction is declare which return the int value 
+{
+	float second, century;
+	// hare is declare the variable
+	printf("Enter second to Covert in century : ");
+	// Hare say to the user to century to Covert in second
+	scanf("%f",&century);
+	//Har get the user input
+	second = century * 3153600000;
+	// hare declare the formula
+	printf("Your Covertion is second = %f second \n", second);
+	// here is result which user wants
+	return 0;
+	// hare is main function return value
+}
+// hare is the finish the main function and Program
