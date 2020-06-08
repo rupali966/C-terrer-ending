eaf9fffa7fe8f8e75a70b61be1efee0ce0789670
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-08 17:52:01 +0530

    Create footPoundtojoule.c

---
+++ C.Operator/Conversion.Example/Energy.Conversion/footPoundtojoule.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // hare call the standerd input output library for input and output
+int main() // hare is the main funnction is declare which return the int value 
+{
+	float joule, footPount;
+	// hare is declare the variable
+	printf("Enter footPound to Covert in joule : ");
+	// Hare say to the user to footPound to Covert in joule
+	scanf("%f",&footPount);
+	//Har get the user input
+	joule = footPount*1.356;
+	// hare declare the formula
+	printf("Your Covertion is jolue = %f J \n",joule);
+	// here is result which user wants
+	return 0;
+	// hare is main function return value
+}
+// hare is the finish the main function and Program
