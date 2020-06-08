7aa21b10a9705055f36e4c72cdff56e7a3fb1db5
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-08 18:01:12 +0530

    Add files via upload

---
+++ C.Operator/Conversion.Example/Pressure.Conversion/paskaltobar.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // hare call the standerd input output library for input and output
+int main() // hare is the main funnction is declare which return the int value 
+{
+	float bar, pascal;
+	// hare is declare the variable
+	printf("Enter pascal to Covert in bar : ");
+	// Hare say to the user to pascal to Covert in bar
+	scanf("%f",&pascal);
+	//Har get the user input
+	bar = pascal/100000;
+	// hare declare the formula
+	printf("Your Covertion is bar = %f bar \n", bar);
+	// here is result which user wants
+	return 0;
+	// hare is main function return value
+}
+// hare is the finish the main function and Program
---
+++ C.Operator/Conversion.Example/Pressure.Conversion/pounpersqureinchtobar.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // hare call the standerd input output library for input and output
+int main() // hare is the main funnction is declare which return the int value 
+{
+	float bar, PFPSI;
+	// hare is declare the variable
+	printf("Enter PFPSI to Covert in bar : ");
+	// Hare say to the user to PFPSI to Covert in bar
+	scanf("%f",&PFPSI);
+	//Har get the user input
+	bar = PFPSI/14.504;
+	// hare declare the formula
+	printf("Your Covertion is bar = %f bar aprox \n", bar);
+	// here is result which user wants
+	return 0;
+	// hare is main function return value
+}
+// hare is the finish the main function and Program
