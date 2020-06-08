53cac268377990710a829b052fb4887c16abaaac
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-08 17:53:31 +0530

    Create kilcalarytojoule.c

---
+++ C.Operator/Conversion.Example/Energy.Conversion/kilcalarytojoule.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // hare call the standerd input output library for input and output
+int main() // hare is the main funnction is declare which return the int value 
+{
+	float joule, Kilocalarie;
+	// hare is declare the variable
+	printf("Enter Kilocalarie to Covert in joule : ");
+	// Hare say to the user to Kilocalarie to Covert in joule
+	scanf("%f",&Kilocalarie);
+	//Har get the user input
+	joule = Kilocalarie*4184;
+	// hare declare the formula
+	printf("Your Covertion is jolue = %f J \n",joule);
+	// here is rsult which user wants
+	return 0;
+	// hare is main function return value
+}
+// hare is the finish the main function and Program
