7da4c088ab2905a2c1ae04c44153923a2acfbef1
rupali966 <48244878+rupali966@users.noreply.github.com>
2020-06-08 18:05:46 +0530

    Create cubicfoottoleter.c

---
+++ C.Operator/Conversion.Example/Volume.Conversion/cubicfoottoleter.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // hare call the standerd input output library for input and output
+int main() // hare is the main funnction is declare which return the int value 
+{
+	float later, cubicfoot;
+	// hare is declare the variable
+	printf("Enter later to Covert in cubicfoot : ");
+	// Hare say to the user to cubicfoot to Covert in later
+	scanf("%f",&cubicfoot);
+	//Har get the user input
+	later = cubicfoot * 28.317;
+	// hare declare the formula
+	printf("Your Covertion is later = %f later aprox \n", later);
+	// here is result which user wants
+	return 0;
+	// hare is main function return value
+}
+// hare is the finish the main function and Program
