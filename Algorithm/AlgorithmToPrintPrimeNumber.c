d8a04770208c17c151b4543c39ab00c618b4df4f
rupali966 <48244878+rupali966@users.noreply.github.com>
2021-01-27 19:44:07 +0530

    Create AlgorithmToPrintPrimeNumber.c
    In computational number theory, a variety of algorithms make it possible to generate prime numbers efficiently. These are used in various applications, for example hashing, public-key cryptography, and search of prime factors in large numbers.
    
    For relatively small numbers, it is possible to just apply trial division to each successive odd number. Prime sieves are almost always faster.

---
+++ Algorithm/AlgorithmToPrintPrimeNumber.c
@@ -0,0 +1,26 @@
+﻿#define N 10000
+
+main ()
+
+{
+
+    int i ,j ,a[N+1];
+
+    for(a[i] = 0; i = 2 && i <= N; i++)
+
+    a[i] = 1;
+
+    for(i = 2; i <= N/2;i++)
+
+    for(j = 2; j <= N/i; j++ )
+
+    a[i*j] = 0;
+
+    for(i = 1; i <= N; i++)
+
+    if (a[i])
+
+        printf(" %4d ", i);
+
+}
+
