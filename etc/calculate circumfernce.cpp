c90cc50aa1ed9f9e541627f053fa7ba3b0855b0c
rupali966 <48244878+rupali966@users.noreply.github.com>
2021-02-03 22:33:25 +0530

    Add files via upload

---
+++ etc/calculate circumfernce.cpp
@@ -0,0 +1,16 @@
+// Circumference and area of a circle with radius 2.5
+
+#include <iostream>
+using namespace std;
+const double pi = 3.141593;
+int main()
+{
+double area, circuit, radius = 1.5;
+area = pi * radius * radius;
+circuit = 2 * pi * radius;
+cout << "\nTo Evaluate a Circle\n" << endl;
+cout << "Radius: " << radius << endl
+<< "Circumference: " << circuit << endl
+<< "Area: " << area << endl;
+return 0;
+}
