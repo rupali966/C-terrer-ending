15a06b2bf6dbe05752f301efb79949bbbcb691c3
rupali966 <48244878+rupali966@users.noreply.github.com>
2021-02-05 19:37:24 +0530

    Update Vilocity.cpp

--- etc/Vilocity.cpp
+++ etc/Vilocity.cpp
@@ -1,43 +1,99 @@
-// area.cpp
-// Example for a simple function returning a value.
-//-----------------------------------------------------
+
 #include <iostream>
+
 #include <iomanip>
+
 using namespace std;
 
+
+
 float ratioOfTime ();
+
 float ratioOfDistance ();
+
+float AverageVilocity ();
+
 // Prototype
+
 int main()
 
+
+
 {
-  float result1, result2;
+
+  float result1, result2, result3;
+
   result1 = ratioOfTime();
+
   result2 = ratioOfDistance();
+
   cout << "Ratio of time = "<< result1<< "\n";
-  cout << "Ration of Displacement  = "<< result2;
+
+  cout << "Ration of Displacement  = "<< result2 << "\n";
+
+cout << "Average Vilocity = "<< result3 << "\n";
+
   return 0;
+
 }
-// Defining the function area():
-// Computes the area of a rectangle.
+
+
+
+
+
 
 
 float ratioOfTime() 
+
 {
+
   float startingTime, endTime;
+
   cout << "Enter Starting Time : "; 
+
   cin >> startingTime; 
+
   cout << "Enter ending Time : "; 
+
   cin >> endTime; 
+
   return (endTime - startingTime);
+
 }
+
+
 float ratioOfDistance() 
+
 {
+
   float startingPoint, endPoint;
+
   cout << "Enter Starting Point : "; 
+
   cin >> startingPoint; 
+
   cout << "Enter end Point : "; 
+
   cin >> endPoint; 
+
   return (endPoint - startingPoint);
+
 }
+
+
+float AverageVilocity() {
+
+  float x, y;
+
+  x = ratioOfTime();
+
+  y = ratioOfDistance();
+
+  return (x/y);
+
+  
+
+}
+
+
 
